42b0a64d07369ba05e46e2840eec07579eaaa3b7
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-08-06 23:50:29 +0530

    feat: update projects section

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -16,6 +16,51 @@ interface Project {
 export const projectsData: Project[] = [
   {
     id: "1",
+    title: "Zeno",
+    description:
+      "Zeno is a modern, full-stack productivity app built with Next.js, Supabase, and TailwindCSS. It helps you manage your tasks, notes, resources, goals, and more—all in one place with user authentication and cloud storage.",
+    image: "/projects/zeno.png",
+    technologies: [
+      "Next.js",
+      "TypeScript",
+      "TailwindCSS",
+      "ShadCN",
+      "Supabase",
+      "Framer Motion",
+    ],
+    category: "Web Development",
+    links: {
+      github: "https://github.com/SrihasRC/Zeno",
+      live: "https://zeno-sr.vercel.app/",
+    },
+    featured: true,
+  },
+  {
+    id: "2",
+    title: "NoirChat",
+    description:
+      "NoirChat is a full-stack chat application featuring real-time messaging, private conversations, and a sleek user interface. Built with modern web technologies, it offers a secure and responsive chat experience.",
+    image: "/projects/noirchat.png",
+    technologies: [
+      "Next.js",
+      "TypeScript",
+      "TailwindCSS",
+      "ShadCN",
+      "Node.js",
+      "Express",
+      "Socket.IO",
+      "MongoDB",
+      "BcryptJS",
+      "JWT",
+    ],
+    category: "Web Development",
+    links: {
+      github: "https://github.com/SrihasRC/NoirChat",
+    },
+    featured: true,
+  },
+  {
+    id: "3",
     title: "HexKit",
     description:
       "HexKit is an all-in-one platform for Capture The Flag (CTF) challenges featuring a comprehensive suite of cryptography, encoding, and analysis tools to help you crack the code and capture the flag.",
@@ -36,7 +81,7 @@ export const projectsData: Project[] = [
     featured: true,
   },
   {
-    id: "2",
+    id: "4",
     title: "Subscription-Tracker",
     description:
       "A Node.js/Express application to help users manage and track their subscriptions, receive reminders, and keep their spending in check. It features user authentication, subscription management, and email notifications.",
@@ -56,10 +101,10 @@ export const projectsData: Project[] = [
     links: {
       github: "https://github.com/SrihasRC/Subscription-Tracker",
     },
-    featured: true,
+    featured: false,
   },
   {
-    id: "3",
+    id: "5",
     title: "SecureShare",
     description:
       "A secure, browser-based peer-to-peer file sharing application built with React and WebRTC. It allows users to share files directly between browsers without a central server, ensuring privacy and speed.",
@@ -77,6 +122,6 @@ export const projectsData: Project[] = [
       github: "https://github.com/SrihasRC/SecureShare",
       live: "https://secureshare11.netlify.app/",
     },
-    featured: true,
+    featured: false,
   },
 ];
